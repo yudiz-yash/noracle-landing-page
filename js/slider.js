26633079d4c83fda7666b186baeac0629533a8cf
yudiz-fenil <fenil.babariya@yudiz.com>
2023-06-21 13:51:01 +0530

    slider script

--- js/slider.js
+++ js/slider.js
@@ -1,3 +1,23 @@
+const isSliderVisible = true;
+const aAnnouncement = [
+    "$NORA Token Pre-Sale Amount",
+    "$NORA Token Pre-Sale Comming Soon",
+    "$NORA Token Pre-Sale Amount",
+    "$NORA Token Pre-Sale Comming Soon",
+]
+const list = document.getElementById("announcement-list");
+aAnnouncement.forEach((value) => {
+    const listItem = document.createElement("div");
+    listItem.textContent = value;
+    list.appendChild(listItem);
+});
+const annoucementsection = document.getElementById("annoucement-section");
+if (isSliderVisible) {
+    annoucementsection.style.display = "block";
+} else {
+    annoucementsection.style.display = "none";
+}
+
 $('.annoucement-slider').slick({
     infinite: true,
     slidesToShow: 3,
